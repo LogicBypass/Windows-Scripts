cb0b43d36da8c3a57ca39a5088aa247351599f24
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2022-12-27 21:09:33 +0200

    Added chkdsk function for each disk

--- Fix Windows Filesystem.bat
+++ Fix Windows Filesystem.bat
@@ -4,3 +4,5 @@ dism /Online /Cleanup-image /CheckHealth
 dism /Online /Cleanup-image /RestoreHealth
 dism /Online /Cleanup-image /StartComponentCleanup
 Sfc /ScanNow
+$LogicalDisks = Get-WmiObject Win32_LogicalDisk | Select-Object -ExpandProperty DeviceID
+foreach ($Disk in $LogicalDisks){chkdsk $Disk /f /r /x }
