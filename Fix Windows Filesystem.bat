8dacef733bcf55de02a37e0f9b61b1a8d5a038c6
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2023-12-14 18:50:32 +0200

    change order

--- Fix Windows Filesystem.bat
+++ Fix Windows Filesystem.bat
@@ -1,8 +1,9 @@
 SC config trustedinstaller start=auto
+Sfc /ScanNow
+dism /Online /Cleanup-image /RestoreHealth
 dism /Online /Cleanup-image /ScanHealth
 dism /Online /Cleanup-image /CheckHealth
 dism /Online /Cleanup-image /RestoreHealth
 dism /Online /Cleanup-image /StartComponentCleanup
-Sfc /ScanNow
 $LogicalDisks = Get-WmiObject Win32_LogicalDisk | Select-Object -ExpandProperty DeviceID
 foreach ($Disk in $LogicalDisks){chkdsk $Disk /f /r /x }
