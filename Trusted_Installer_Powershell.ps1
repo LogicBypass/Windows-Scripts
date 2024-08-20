1d87c4bfb5a9213b080faebe337d64ed4703b1a6
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2024-08-21 00:38:46 +0300

    Update Trusted_Installer_Powershell.ps1

--- Trusted_Installer_Powershell.ps1
+++ Trusted_Installer_Powershell.ps1
@@ -1,4 +1,9 @@
-﻿Install-Module -Name NtObjectManager -RequiredVersion 1.1.32
+if (-not ([bool]([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+Install-Module -Name NtObjectManager -RequiredVersion 1.1.32
 Import-Module NtObjectManager
 Start-Service TrustedInstaller
 $p = Get-NtProcess TrustedInstaller.exe
