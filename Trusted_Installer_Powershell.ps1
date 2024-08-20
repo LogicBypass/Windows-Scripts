19895743ec6fe3914cd45b1e2f2f1b91091ce10a
LogicBypass <62214984+LogicBypass@users.noreply.github.com>
2024-08-21 00:22:40 +0300

    Trusted installer powershell script

---
+++ Trusted_Installer_Powershell.ps1
@@ -0,0 +1,5 @@
+﻿Install-Module -Name NtObjectManager -RequiredVersion 1.1.32
+Import-Module NtObjectManager
+Start-Service TrustedInstaller
+$p = Get-NtProcess TrustedInstaller.exe
+$proc = New-win32process powershell.exe -Creationflags NewConsole -ParentProcess $p
